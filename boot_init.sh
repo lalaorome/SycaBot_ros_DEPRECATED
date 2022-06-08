63dbddc03359857fe948e2f48cd8db808eb79765
Sycamore <pierre.chassagne@epfl.ch>
2022-06-08 12:16:24 +0200

    Delete 1st commit
    Delete to make this package the one for Sycabot.

--- boot_init.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd syca_ws
-source docker/run.sh
-source install/local_setup.bash
-ros2 launch jetbot init.launch.py
--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-ros_workspace_setup="$WORKSPACE_ROOT/install/local_setup.bash"
-echo "sourcing   $ros_workspace_setup"
-source "$ros_workspace_setup"
-
-exec "$@"
