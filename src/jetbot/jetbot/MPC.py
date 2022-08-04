ed48e42f32246b09b2d831bc5cb7e706f7229fb7
Sycamore <pierre.chassagne@epfl.ch>
2022-08-04 09:37:11 +0200

    R -> R_mat

--- src/jetbot/jetbot/MPC.py
+++ src/jetbot/jetbot/MPC.py
@@ -41,7 +41,7 @@ class MPC(CtrllerActionServer):
         self.Tf = self.get_parameter('horizon').value
 
         self.viz_pathref_pub = self.create_publisher(Pose2D, f'/SycaBot_W{self.id}/visualisation', 10)
-        self.ocp_solver = self.config_ocp(self.Q,self.R)
+        self.ocp_solver = self.config_ocp(self.Q,self.R_mat)
 
     def control_cb(self, goal_handle):
         result = Control.Result()
